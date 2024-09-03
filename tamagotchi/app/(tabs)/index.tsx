ecc67a11fac7c1baf54a769c0ed8d92142ea6647
Gabriel MR <gmonteiro810@gmail.com>
2024-09-03 13:30:49 -0300

    criando o images

--- tamagotchi/app/(tabs)/index.tsx
+++ tamagotchi/app/(tabs)/index.tsx
@@ -1,13 +1,52 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import { useRouter } from 'expo-router';
+import characterImagesAPI, { CharacterId } from '../../assets/characters/images';
 
 export default function IndexScreen() {
   const router = useRouter();
 
+  const characterIds: CharacterId[] = [1, 2, 3, 4, 5];
+
+  // Testando a função getAllCharacterImages
+  const allImages = characterImagesAPI.getAllCharacterImages();
+
+  // Testando a função getImagesByState para o estado 'muitofeliz'
+  const muitofelizImages = characterImagesAPI.getImagesByState('muitofeliz');
+
+  // Testando a função getImageByCharacterAndState para o personagem 1 e estado 'muitofeliz'
+  const arkanaMuitofelizImage = characterImagesAPI.getImageByCharacterAndState(1, 'muitofeliz');
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Tela Inicial</Text>
+
+      {/* Testando getAllCharacterImages */}
+      <Text>Testando getAllCharacterImages:</Text>
+      {characterIds.map((id) => (
+        <Image
+          key={`allImages-${id}`}
+          source={allImages[id]['muitofeliz']}
+          style={{ width: 50, height: 50, margin: 5 }}
+        />
+      ))}
+
+      {/* Testando getImagesByState */}
+      <Text>Testando getImagesByState para 'muitofeliz':</Text>
+      {muitofelizImages.map((image, index) => (
+        <Image
+          key={`muitofeliz-${index}`}
+          source={image}
+          style={{ width: 50, height: 50, margin: 5 }}
+        />
+      ))}
+
+      {/* Testando getImageByCharacterAndState */}
+      <Text>Testando getImageByCharacterAndState para personagem 1 e estado 'muitofeliz':</Text>
+      <Image
+        source={arkanaMuitofelizImage}
+        style={{ width: 100, height: 100, margin: 10 }}
+      />
     </View>
   );
 }
--- tamagotchi/assets/characters/images.ts
+++ tamagotchi/assets/characters/images.ts
@@ -1,28 +1,57 @@
-const characterMap = {
-    1: 'arkana',
-    2: 'ishizu',
-    3: 'joey',
-    4: 'kaiba',
-    5: 'mako',
-  };
-  
-  const states = ['bem', 'muitofeliz', 'muitotriste', 'ok', 'triste', 'critico'];
-  
-  const generateCharacterImages = (characterMap: { [id: number]: string }, states: string[]) => {
-    const images: { [id: number]: { [key: string]: any } } = {};
-  
-    Object.entries(characterMap).forEach(([id, character]) => {
-      images[parseInt(id)] = {};
-  
-      states.forEach(state => {
-        images[parseInt(id)][state] = require(`./${character}/${state}.png`);
-      });
-    });
-  
-    return images;
-  };
-  
-  const characterImages = generateCharacterImages(characterMap, states);
-  
-  export default characterImages;
-  
+export type CharacterState = 'bem' | 'muitofeliz' | 'muitotriste' | 'ok' | 'triste' | 'critico';
+export type CharacterId = 1 | 2 | 3 | 4 | 5;
+
+const characterImages: Record<CharacterId, Record<CharacterState, any>> = {
+  1: {
+    bem: require('./arkana/bem.png'),
+    muitofeliz: require('./arkana/muitofeliz.png'),
+    muitotriste: require('./arkana/muitotriste.png'),
+    ok: require('./arkana/ok.png'),
+    triste: require('./arkana/triste.png'),
+    critico: require('./arkana/muitotriste.png'),
+  },
+  2: {
+    bem: require('./ishizu/bem.png'),
+    muitofeliz: require('./ishizu/muitofeliz.png'),
+    muitotriste: require('./ishizu/muitotriste.png'),
+    ok: require('./ishizu/ok.png'),
+    triste: require('./ishizu/triste.png'),
+    critico: require('./ishizu/muitotriste.png'),
+  },
+  3: {
+    bem: require('./joey/bem.png'),
+    muitofeliz: require('./joey/muitofeliz.png'),
+    muitotriste: require('./joey/muitotriste.png'),
+    ok: require('./joey/ok.png'),
+    triste: require('./joey/triste.png'),
+    critico: require('./joey/muitotriste.png'),
+  },
+  4: {
+    bem: require('./kaiba/bem.png'),
+    muitofeliz: require('./kaiba/muitofeliz.png'),
+    muitotriste: require('./kaiba/muitotriste.png'),
+    ok: require('./kaiba/ok.png'),
+    triste: require('./kaiba/triste.png'),
+    critico: require('./kaiba/muitotriste.png'),
+  },
+  5: {
+    bem: require('./mako/bem.png'),
+    muitofeliz: require('./mako/muitofeliz.png'),
+    muitotriste: require('./mako/muitotriste.png'),
+    ok: require('./mako/ok.png'),
+    triste: require('./mako/triste.png'),
+    critico: require('./mako/muitotriste.png'),
+  },
+};
+
+export default {
+  getAllCharacterImages: () => characterImages,
+
+  getImagesByState: (state: CharacterState) => {
+    return Object.values(characterImages).map(images => images[state]);
+  },
+
+  getImageByCharacterAndState: (characterId: CharacterId, state: CharacterState) => {
+    return characterImages[characterId][state];
+  },
+};
